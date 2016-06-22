8cedc416a56cc7c3a3de0c9dd20ea40177780d16
Kyle Hamburg <kylelhamburg@gmail.com>
2016-06-22 11:46:02 -0600

    Added getTweets component

---
+++ components/main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var GetTweets = require('./getTweets.js');
+var Main = React.createClass({
+  render:function(){
+    return (
+      <div><h1>You found the main page!</h1>
+        
+      </div>
+    )
+  }
+})
+
+module.exports = Main;
