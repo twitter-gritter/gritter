56d07c9620d3c7baade323953e2737f994c591cc
Mike Hohne <mikehohne@gmail.com>
2016-06-22 10:04:23 -0600

    got webpack working.

--- server.js
+++ server.js
@@ -1,38 +1,59 @@
+
 var express = require('express');
-var app = express();
+var cors = require('cors');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var session = require('express-session');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tweets');
-var tweetRouter = require('./routes/tweets');
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static(__dirname + '/views'));
 
-var port = process.env.PORT || 8000;
-var router = express.Router();
 
 
-router.use(function(req, res, next){
-	console.log('Something is happening');
-	next();
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Running in production mode');
+
+  app.use('/static', express.static('static'));
+} else {
+  // When not in production, enable hot reloading
+
+  var chokidar = require('chokidar');
+  var webpack = require('webpack');
+  var webpackConfig = require('./webpack.config.dev');
+  var compiler = webpack(webpackConfig);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
+  app.use(require('webpack-hot-middleware')(compiler));
+
+  // Do "hot-reloading" of express stuff on the server
+  // Throw away cached modules and re-require next time
+  // Ensure there's no important state in there!
+  var watcher = chokidar.watch('./server');
+  watcher.on('ready', function() {
+    watcher.on('all', function() {
+      console.log('Clearing /server/ module cache from server');
+      Object.keys(require.cache).forEach(function(id) {
+        if (/\/server\//.test(id)) delete require.cache[id];
+      });
+    });
+  });
+}
+
+mongoose.connect("mongodb://localhost:27017/gritter");
+mongoose.connection.once('open', function(){
+	console.log("Connected to your database.");
 });
-
 
 app.get('/', function(req, res){
-	res.render('index')
+	res.render('index');
 });
 
-
-
-app.use('/api/tweets', tweetRouter);
-
-
-
-
-app.listen(port);
-console.log("Cash money on port " + port);
+app.listen(7000, function(){
+	console.log("The magic happens on port 7000");
+});
