1768745b18b2bc5742a32da4753ade71bd681e17
Mike Hohne <mikehohne@gmail.com>
2016-06-23 15:29:16 -0600

    fucking static

--- components/grid.js
+++ components/grid.js
@@ -1,28 +1,25 @@
-// GetTweets
-//  Grid
-//    Cards
-
+//Router  calls in -->
+//  Main    calls in -->
+//    GetTweets   calls in -->
+//      TwitterCard   calls in -->
+//        Grid
 var React = require('react');
 var ReactGridLayout = require('react-grid-layout');
-
-var Cards = require('./cards.js');
 var Grid = React.createClass({
   render: function() {
     // layout is an array of objects, see the demo for more complete usage
     var layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 4, minH: 2},
       {i: 'c', x: 4, y: 0, w: 1, h: 2}
     ];
-    console.log(this.props.tweets);
-    // var tweets = this.props.tweets.map(function(item){
-    //   console.log(item)
-    // });
     return (
       <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key={"a"}>Boom!</div>
-        <div key={"b"}>Woosh!</div>
-        <div key={"c"}>We're doomed!</div>
+        <div className="pretty" key={'c'}>
+          <img src={this.props.profile_img}/><h3>{this.props.author}:</h3>
+          <h4>"{this.props.text}..."</h4>
+        </div>
+
       </ReactGridLayout>
     )
   }
