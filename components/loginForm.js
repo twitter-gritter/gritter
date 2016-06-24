c571411570526bcafc1903a8f88bc28dcf5707fe
Mike Hohne <mikehohne@gmail.com>
2016-06-24 11:40:58 -0600

    passport is working again, had to change <forms> around

--- components/loginForm.js
+++ components/loginForm.js
@@ -11,18 +11,20 @@ var LoginForm = React.createClass({
         <hr></hr>
         <h2>New Users </h2>
             <form  id="login" className="form-group loginForm" action = '/login' method = 'POST'>
-            <li><input type="text" className = "form-control" id="user" name="firstName" placeholder="First Name" /></li>
-            <li><input type="text" className = "form-control" id="user" name="lastName" placeholder="Last Name" /></li>
-            <li><input type="username" className = "form-control" id="user" name="email" placeholder="Email" /></li>
-            <li><input type="password" className = "form-control" name="password" id="user" placeholder="Password" /></li>
-            <li><button id="user" className="btn btn-success loginButton">Sign Up</button></li>
-            <h2>Already signed up?</h2>
-            <li><input type="username" className = "form-control" id="user" name="email" placeholder="Email" /></li>
-            <li><input type="password" className = "form-control" name="password" id="user" placeholder="password" /></li>
-            <li><button id="user" className="btn btn-success loginButton">Login</button></li>
+              <li><input type="text" className = "form-control" id="user" name="firstName" placeholder="First Name" /></li>
+              <li><input type="text" className = "form-control" id="user" name="lastName" placeholder="Last Name" /></li>
+              <li><input type="username" className = "form-control" id="user" name="email" placeholder="Email" /></li>
+              <li><input type="password" className = "form-control" name="password" id="user" placeholder="Password" /></li>
+              <li><button id="user" className="btn btn-success loginButton">Sign Up</button></li>
             </form>
+            <h2>Already signed up?</h2>
+            <form  id="login" className="form-group loginForm" action = '/login' method = 'POST'>
+              <li><input type="username" className = "form-control" id="user" name="email" placeholder="Email" /></li>
+              <li><input type="password" className = "form-control" name="password" id="user" placeholder="password" /></li>
+              <li><button id="user" className="btn btn-success loginButton">Login</button></li>
+            </form>
+
             <form className ="form-group" action="/logout" method="GET">
-            <hr></hr>
               <li><button id="logoutButton" className="btn btn-warning">Log Out</button></li>
             </form>
           </ul>
