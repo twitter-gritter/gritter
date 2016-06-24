83da21ac67f5f996998203ca4a01fc58a96fba21
Mike Hohne <mikehohne@gmail.com>
2016-06-24 12:15:48 -0600

    lunch

--- components/loginForm.js
+++ components/loginForm.js
@@ -10,12 +10,12 @@ var LoginForm = React.createClass({
         <h2>Login or Sign Up Below</h2>
         <hr></hr>
         <h2>New Users </h2>
-            <form  id="login" className="form-group loginForm" action = '/login' method = 'POST'>
+            <form onSubmit = { this.onSumbit } id="login" className="form-group loginForm" action = '/login' method = 'POST'>
               <li><input type="text" className = "form-control" id="user" name="firstName" placeholder="First Name" /></li>
               <li><input type="text" className = "form-control" id="user" name="lastName" placeholder="Last Name" /></li>
               <li><input type="username" className = "form-control" id="user" name="email" placeholder="Email" /></li>
               <li><input type="password" className = "form-control" name="password" id="user" placeholder="Password" /></li>
-              <li><button id="user" className="btn btn-success loginButton">Sign Up</button></li>
+              <li><button  id="user" className="btn btn-success loginButton">Sign Up</button></li>
             </form>
             <h2>Already signed up?</h2>
             <form  id="login" className="form-group loginForm" action = '/login' method = 'POST'>
