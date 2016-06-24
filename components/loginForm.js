532741d5b09e8ef15c2f5236dbd43b3ee63608aa
Mike Hohne <mikehohne@gmail.com>
2016-06-24 10:50:36 -0600

    got form working

--- components/loginForm.js
+++ components/loginForm.js
@@ -4,7 +4,7 @@ var LoginForm = React.createClass({
   render: function(){
     return(
       <div>
-        <div className = "dropdown loginDropdown">
+        <div id="dropdown" className = "dropdown loginDropdown">
         <button className ="btn btn-success dropdown-toggle" type="button" data-toggle="dropdown">Sign Up/Login<span className="caret"></span></button>
         <ul className="dropdown-menu dropdown-menu-right">
             <form  id="login" className="form-group loginForm" action = '/login' method = 'POST'>
@@ -13,6 +13,9 @@ var LoginForm = React.createClass({
             <li><input type="username" className = "form-control" id="userName" name="email" placeholder="email/username" /></li>
             <li><input type="password" className = "form-control" name="password" id="userPassword" placeholder="password" /></li>
             <li><button id="loginButton" className="btn btn-success">Sign Up</button></li>
+            </form>
+            <form className ="form-group" action="/logout" method="GET">
+              <li><button id="logoutButton" className="btn btn-warning">Log Out</button></li>
             </form>
           </ul>
         </div>
