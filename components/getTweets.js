08fec887ae89219ce9034b058a202ebb6b1fd932
Mike Hohne <mikehohne@gmail.com>
2016-06-28 14:19:25 -0600

    fixed container

--- components/getTweets.js
+++ components/getTweets.js
@@ -50,7 +50,7 @@ var GetTweets = React.createClass({
   render: function () {
     this.addId();
   return (
-    <div className = "container searchContainer">
+    <div className = "container">
       <div className="keywordInput">
         <p> Search by keyword: {decodeURIComponent(this.state.keyword)}</p>
         <SearchBar onKeywordSubmit={this.onKeywordSubmit}/>
