a052b74fec11409a2582e0b3548e8440a649a02c
Chalice Stevens <chaliceariel@gmail.com>
2016-06-28 10:55:56 -0600

    decode URI text above search bar

--- components/getTweets.js
+++ components/getTweets.js
@@ -45,7 +45,7 @@ var GetTweets = React.createClass({
   return (
     <div className = "container searchContainer">
       <div className="keywordInput">
-        <p> Search by keyword: {this.state.keyword}</p>
+        <p> Search by keyword: {decodeURIComponent(this.state.keyword)}</p>
         <SearchBar onKeywordSubmit={this.onKeywordSubmit}/>
         <div className="twitterGrid">
           <TwitterCard tweetsArr={this.state.tweets} />
