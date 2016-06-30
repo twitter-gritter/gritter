8394876b031de1c93ea60746c4ea8826533e1df5
Mike Hohne <mikehohne@gmail.com>
2016-06-30 14:27:11 -0600

    merging

--- components/grid.js
+++ components/grid.js
@@ -33,8 +33,8 @@ var Grid = React.createClass({
       <div id="twitCardHolder" key={el.id} _grid={{x:el.id * 4 % 12,y:Infinity,w:4,h:5}} >
         <div id="deleteButton" onClick= {this.props.removeTweet.bind(null, el.id)}><CircleEx /></div>
         <div id="lock"><Lock /></div>
-        <div id="twitterLogo"><TwitterLogo /></div>
         <img id="tweetImage" src={el.profile_img}/>
+        <div id="twitterLogo"><TwitterLogo /></div>
         <h4 id="screenName">{el.screen_name}:</h4>
         <h5 id="tweetFont">"{el.text}..."</h5>
       </div>
--- components/searchBar.js
+++ components/searchBar.js
@@ -49,11 +49,11 @@ var SearchBar = React.createClass({
 					 value={this.state.keyword} type="text" autoComplete="off" id="inputKeyword" />
 					 <div className="counterDiv">
 					 <input onChange={this.handleNumberChange}
-					 value={this.state.number}  type= "text" min="1" max="25" id="inputNumber" />
+					 value={this.state.number}  type= "text" autoComplete="off" min="" max="25" id="inputNumber" />
 					 <div>
 						<button className="btn searchButton"><FaArrowRight /></button>
 					</div>
-					</div>
+				 </div>
 				</form>
 		</div>
 			)
