06df08b3f42d7acbf7dca52a9fb83c97102a1c84
Kyle Hamburg <kylelhamburg@gmail.com>
2016-06-30 15:03:34 -0600

    Working on layout

--- components/grid.js
+++ components/grid.js
@@ -7,6 +7,9 @@ var ReactGridLayout = require('react-grid-layout');
 var _ = require('lodash');
 var CircleEx = require('react-icons/lib/fa/times-circle.js');
 var TwitterLogo = require('react-icons/lib/fa/twitter.js');
+var WidthProvider = require('react-grid-layout').WidthProvider;
+    ReactGridLayout = WidthProvider(ReactGridLayout);
+
 
 
 var TwitterCard = require('./twitterCard.js');
@@ -15,20 +18,41 @@ var Grid = React.createClass({
   getDefaultProps(){
     return {
       className: "layout",
-      items: 10,
+      items: 50,
       cols: 12,
       rowHeight: 30,
-      verticalCompact:false
+      layout: []
     }
   },
   makeLayout(){
-
+    var p = this.props;
+    return _.map(p.tweets, function(tweet, i){
+      return {
+        x: i * 2 % 12, y: Math.floor(i / 6), w:4, h:5, i:tweet.id.toString()
+      };
+    })
+  },
+  makeTweetCards(){
+    var layout = this.state.layout;
+    //this.setState({layout:layout})
+    return _.map(layout, function(l){
+      return (
+        <div key={l.i} _grid={l}>
+          <div>{l.i}</div>
+        </div>
+      )
+    })
+  },
+  onBreakpointChange(breakpoint, cols){
+    this.setState({
+      breakpoint: breakpoint,
+      cols: cols
+    })
   },
   onLayoutChange: function(layout){
     this.setState({layout: layout});
   },
   createElement(el){
-
     return(
       <div id="twitCardHolder" key={el.id} _grid={{x:el.id * 4 % 12,y:Infinity,w:4,h:5}} >
         <div id="twitterLogo"><TwitterLogo /></div>
@@ -44,17 +68,15 @@ var Grid = React.createClass({
       </div>
     )
   },
+  componentDidMount(){
+    var layout = this.makeLayout();
+    this.setState({layout:layout})
+    console.log(layout)
+  },
   render: function() {
-    // layout is an array of objects, see the demo for more complete usage
-    var layout = [
-      {i: "1", x: 0, y: 0, w: 1, h: 2, static: true},
-      {i: '2', x: 1, y: 0, w: 3, h: 2, minW: 2, minH: 2},
-      {i: '3', x: 4, y: 0, w: 1, h: 2}
-    ];
-
     return (
-      <ReactGridLayout  layout={layout} cols={12} rowHeight={30} width={1200}>
-          {_.map(this.props.tweets, this.createElement)}
+      <ReactGridLayout  onLayoutChange={this.onLayoutChange} onBreakpointChange={this.onBreakpointChange} {...this.props} >
+          {this.makeTweetCards()}
       </ReactGridLayout>
     )
   }
