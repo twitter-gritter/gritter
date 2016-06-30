07dc77eb3db536ac77298030d0c70444dc047dc6
Chalice Stevens <chaliceariel@gmail.com>
2016-06-30 11:56:15 -0600

    fixed number input and added RegEx and error handling, filter tweets by English language

--- components/searchBar.js
+++ components/searchBar.js
@@ -8,7 +8,7 @@ var SearchBar = React.createClass({
 	getInitialState: function(){
 		return {
 			newKeyword: '',
-			newNumber: 1
+			newNumber: 0
 		}
 	},
 	handleKeywordChange: function(event){
@@ -16,11 +16,18 @@ var SearchBar = React.createClass({
 			newKeyword: event.target.value
 		})
 	},
-	handleNumberChange: function(event){
-		this.setState({
-			newNumber: event.target.value
-		})
+	handleNumberChange: function(event){	
+		var input = new RegExp('^([0-9]|[0-9][0-9])$');
+		if (event.target.value > 25) {
+			alert('Too many tweets! Please enter a number between 1 and 25.');
+		} else if (event.target.value < 1 || input.test(event.target.value) === false) {
+			alert('Invalid input. Please enter a number between 1 and 25.');
+		} 
+			 
 
+		this.setState({
+				newNumber: event.target.value
+			})
 	},
 	handleFormSubmit: function(event){
 		event.preventDefault();
@@ -43,7 +50,7 @@ var SearchBar = React.createClass({
 					 value={this.state.keyword} type="text" autoComplete="off" id="inputKeyword" />
 
 					 <input onChange={this.handleNumberChange}
-					 value={this.state.number} type= "number" min="1" max="50" id="inputNumber" />
+					 value={this.state.number}  type= "text" min="1" max="25" id="inputNumber" />
 
 					 <div>
 					<button className="btn searchButton"><FaArrowRight /></button>
