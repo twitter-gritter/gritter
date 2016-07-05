74cb000f67c31d9de58f5c284b5f450f4ad8782e
Kyle Hamburg <kylelhamburg@gmail.com>
2016-07-05 10:22:03 -0600

    Removed stray semicolon

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 mongoose.connect(
-  config.mongo_uri;
+  config.mongo_uri
 //  "mongodb://localhost:27017/gritter"
 );
 mongoose.connection.once('open', function(){
