4d7e38518ab73ebc76b11483e64d8be55eba3aaa
Chalice Stevens <chaliceariel@gmail.com>
2016-07-07 16:16:42 -0600

    responsive styles

--- components/clearButton.js
+++ components/clearButton.js
@@ -4,7 +4,7 @@ var CircleEx = require('react-icons/lib/fa/times-circle.js');
 var ClearButton = React.createClass({
   render: function(){
     return(
-      <button id="clear" onClick={this.props.clearTweets}><CircleEx /></button>
+      <button id="clear" onClick={this.props.clearTweets} data-toggle="tooltip" data-placement="left"><CircleEx /></button>
     )
   }
 })
--- components/grid.js
+++ components/grid.js
@@ -9,6 +9,7 @@ var CircleEx = require('react-icons/lib/fa/times-circle.js');
 var LinkIcon = require('react-icons/lib/fa/chain.js');
 var TwitterLogo = require('react-icons/lib/fa/twitter.js');
 var WidthProvider = require('react-grid-layout').WidthProvider;
+var Lock = require('react-icons/lib/fa/lock.js');
     ReactGridLayout = WidthProvider(ReactGridLayout);
 var moment = require('moment');
 
@@ -36,37 +37,28 @@ var Grid = React.createClass({
   },
   removeLinks: function(text){
     var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-<<<<<<< HEAD
+
     //remove strings of text from body of tweet that are urls, i.e. match the RegEx
     return text.replace(urlRegex, ''); 
-=======
-    return text.replace(urlRegex, '');
->>>>>>> origin/master
-  },
-  replaceInternalLinks: function(text){
-
   },
   findUrls: function(array){
-    //if tweet.entities.urls (which is an array) is not empty, i.e. there ARE links
+    //if tweet.entities.urls (which is an array) is not empty, i.e. there ARE links...
     if(array[0] !== null && array[0] !== undefined){
       //return object with values for full url and a truncated url
       return {
         url: array[0].expanded_url,
         display_url: array[0].display_url
       }
-<<<<<<< HEAD
+
     } 
       //else return values as empty strings; will not be displayed
-=======
-    }
-
->>>>>>> origin/master
       return {
         url: '',
         display_url: ''
       }
   },
   displayLinks: function(links){
+    //only display link icon if links exist
     if(links.url.length > 0){
       return(
         <a id="links" target="_blank" href={links.url}><LinkIcon /> {links.display_url}</a>
@@ -75,25 +67,22 @@ var Grid = React.createClass({
   },
   createElement(el){
 
-    var date = moment(el.created_at).format('D MMMM YYYY h:m a');
+    var date = moment(el.created_at).format('D MMMM YYYY h:mm a');
     var profileLink = "https://twitter.com/" + el.screen_name;
     var body = this.removeLinks(el.text);
     var links = this.findUrls(el.urls);
     return(
       <div id="twitCardHolder" key={el.id} _grid={{x:el.id * 4 % 12,y:Infinity,w:4,h:5,minW:2}} >
+        <h5 id="date"> {date} </h5>
         <div id="iconDiv" >
+          <div id="lock"><Lock /></div>
           <div id="deleteButton" onClick= {this.props.removeTweet.bind(null, el.id)}><CircleEx /></div>
         </div>
         <a target="_blank" href={profileLink}><img id="profileImage" src={el.profile_img}/></a>
-        <div id="twitterLogo"><TwitterLogo /></div>
-        <h4 id="screenName"><a target="_blank" href={profileLink}>{el.screen_name}</a>:</h4>
-        <h5 id="date"> {date} </h5>
-<<<<<<< HEAD
-        <h5 id="tweetFont">"{body}"</h5>
-        <a id="links" target="_blank" href={links.url}><LinkIcon /> {links.display_url}</a>
-=======
-        <h5 id="tweetFont">"{body}"</h5>{this.displayLinks(links)}
->>>>>>> origin/master
+        <h4 id="screenName"><a target="_blank" href={profileLink}>{el.screen_name}:</a></h4>
+        <div id="tweetBody"><h5 id="tweetText">"{body}"</h5></div>
+        {this.displayLinks(links)}
+        <div id="twitterLogo"><a target="_blank" href="https://twitter.com/"><TwitterLogo /></a></div>
       </div>
     )
   },
