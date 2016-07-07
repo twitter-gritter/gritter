937907cc69d9e03b5e141fa1bb6a83889678e887
Kyle Hamburg <kylelhamburg@gmail.com>
2016-07-07 10:01:08 -0600

    Sendinging Twitter Auth build to heroku

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ if (process.env.NODE_ENV === 'production') {
 
 mongoose.connect(
   config.mongo_uri
-  //"mongodb://localhost:27017/gritter"
+//  "mongodb://localhost:27017/gritter"
 );
 mongoose.connection.once('open', function(){
 	console.log("Connected to your database.");
